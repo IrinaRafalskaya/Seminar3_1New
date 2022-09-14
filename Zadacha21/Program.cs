4316949ee0c1e1b0a8766e83af6d3d6c38e352ef
Irina Rafalskaya <irena.narod@mail.ru>
2022-09-14 21:44:38 +0300

    Distance in 3D space

--- Zadacha21/Program.cs
+++ Zadacha21/Program.cs
@@ -12,10 +12,11 @@ Console.WriteLine("Введите Y2: ");
 int y2 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите Z2: ");
 int z2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(GetDistance(x1, y1, z1, x2, y2, z2));
 
-double GetDistance (int x1, int y1, int z1, int x2, int y2, int z2,)
+double GetDistance (int x1, int y1, int z1, int x2, int y2, int z2)
 {
     double distance = 0;
-    distance = Math.Sqrt(Math.Pow(x2-x1,2) + Math.Pow(y2-y1,2) + Math.Pow(z2-z1,2));
+    distance = Math.Sqrt(Math.Pow(x2-x1, 2) + Math.Pow(y2-y1, 2) + Math.Pow(z2-z1, 2));
     return distance;
 }
