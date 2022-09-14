235b5fb9365a6a219176850ac1d21274d58f2aff
Irina Rafalskaya <irena.narod@mail.ru>
2022-09-14 22:00:54 +0300

    created table of cubes

---
+++ Zadacha23/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Tableofcubes(number);
+
+void Tableofcubes(int number)
+{
+    int count = 1;
+    while(count <= number)
+    {
+        Console.WriteLine($"Число в кубе {count} = {count*count*count}");
+        count++;
+    }
+}
