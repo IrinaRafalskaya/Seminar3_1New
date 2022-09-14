eadaabb90cd4060da162b537bd4d0a513266b89f
Irina Rafalskaya <irena.narod@mail.ru>
2022-09-14 20:27:23 +0300

    Number is Palindrome or not

---
+++ Zadacha19/Program.cs
@@ -0,0 +1,20 @@
+﻿int num, rem, sum = 0, temp;
+    Console.Write("Введите число: ");
+    num = Convert.ToInt32(Console.ReadLine());
+    temp = num;
+        while (Convert.ToBoolean(num))
+            {
+            rem = num % 10;  
+            num = num / 10; 
+            sum = sum * 10 + rem; 
+            }
+            Console.WriteLine("Перевернутое число это: {0} ", sum);
+        if (temp == sum) 
+            {
+            Console.WriteLine("Палиндром");
+            }
+        else
+            {
+            Console.WriteLine("Это не палиндром");
+            }
+        Console.ReadLine();
