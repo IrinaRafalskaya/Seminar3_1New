ea1fc4ffbd893e9a4b00d87749a2e7f6a0f32f37
Irina Rafalskaya <irena.narod@mail.ru>
2022-09-17 17:49:38 +0300

    natural power

---
+++ Zadacha25/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+Console.WriteLine("Введите число A --> ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B --> ");
+int b = Convert.ToInt32(Console.ReadLine());
+int pow = a;
+
+for (int i = 1; i < b; i++)
+{
+    pow = pow * a;
+}
+Console.WriteLine("Число A в натуральной степени B равно: " + pow);
