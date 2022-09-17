372aaa1ba7e1c97b3adb814edda2ca2f06d4d449
Irina Rafalskaya <irena.narod@mail.ru>
2022-09-17 17:19:02 +0300

    Возводит число A в натуральную степень

---
+++ Zadacha29/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+int size = 7;
+int[] array = new int[7];
+Random myRandom = new Random();
+
+Console.WriteLine("Вывод массива");
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = myRandom.Next(0, 7);
+    Console.WriteLine("{0} ", array[i]);
+}
