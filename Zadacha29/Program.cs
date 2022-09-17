66b801e43d8c261710c4cd55afb18f9d5107111b
Irina Rafalskaya <irena.narod@mail.ru>
2022-09-17 17:48:04 +0300

    Print random array

--- Zadacha29/Program.cs
+++ Zadacha29/Program.cs
@@ -1,12 +1,11 @@
 ﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
 
-int size = 7;
-int[] array = new int[7];
+int[] array = new int[8];
 Random myRandom = new Random();
 
-Console.WriteLine("Вывод массива");
+Console.WriteLine("Вывод массива: ");
 for (int i = 0; i < array.Length; i++)
 {
-    array[i] = myRandom.Next(0, 7);
+    array[i] = myRandom.Next(0, 8);
     Console.WriteLine("{0} ", array[i]);
 }
