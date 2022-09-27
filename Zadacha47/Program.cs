4eb7fc045f4c30f11f0df3d5732a5bf87127aab5
Irina Rafalskaya <irena.narod@mail.ru>
2022-09-27 15:08:14 +0300

    wo-dimensional array with real numbers

---
+++ Zadacha47/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами. m = 3, n = 4.
+
+double[,] array = new double[3, 4];
+
+for (int i = 0; i < 3; i++)
+{
+    for (int j = 0; j < 4; j++)
+    {
+        array[i, j] = Convert.ToDouble(new Random().Next(-100, 100)) / 10;
+        Console.Write($"{array[i, j]} ");
+    }
+    Console.WriteLine();
+}
